c4191474f5f3b157d25c1bc3c0c62f45890b1d97
hpierce1312 <57736946+hpierce1312@users.noreply.github.com>
2021-01-06 20:19:01 -0500

    set up youtube api

---
+++ videos/src/components/SearchBar.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+class SearchBar extends React.Component {
+    state = { term: '' };
+
+    onInputChange = (event) => {
+        this.setState({ term: event.target.value });
+    };
+
+    onFormSubmit = (event) => {
+        event.preventDefault(); //prevents the browser from automatically trying to submit the form any time the user submits it
+        this.props.onFormSubmit(this.state.term); //call callback from parent component
+    };
+
+    render() {
+        return (
+            <div className="search-bar ui segment">
+                <form onSubmit={this.onFormSubmit} className="ui form">
+                    <div className="field">
+                        <input 
+                            type="text" 
+                            placeholder="Search..." 
+                            value={this.state.term}
+                            onChange={this.onInputChange}
+                        />
+                    </div>
+                </form>
+            </div>
+        );
+    }
+}
+
+export default SearchBar;
